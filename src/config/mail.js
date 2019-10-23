95fe59b604b2cfbd85c1d477c8a23fec0c1f56e2
Rafael Pallisser de Oliveira <rafaelpallisser@gmail.com>
2019-10-23 00:47:37 -0300

    Configurando Nomemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fb16557a9423c8',
+    pass: 'a45776583ab662',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
